256ac2f8835e825f71230e07c95e0881afb8554c
TalhaTR7 <www.talharafat@gmail.com>
2024-11-13 11:29:42 +0500

    created Card.jsx

--- airbnb/src/Card.jsx
+++ airbnb/src/Card.jsx
@@ -1,9 +1,21 @@
 import './css/Card.css'
+import house_template from "./assets/house-template.png";
 
 export default function Section({ property }) {
     return (
-        <div>
-            <img src={property.image} />
+        <div class="container">
+            <div class="image">
+                <img src={house_template} />
+            </div>
+            <div class="details">
+                <h1>{property.address}, {property.city}</h1>
+                <h3>{property.description}</h3>
+                <h2>{property.vacancy[0]}</h2>
+                <div>
+                    <h2>{property.rent} Night</h2>
+                    <span>★{property.rating}</span>
+                </div>
+            </div>
         </div>
     );
 }
