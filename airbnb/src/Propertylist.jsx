413e9c2f8851ddce3a06fef690081001e5793408
TalhaTR7 <www.talharafat@gmail.com>
2024-11-14 20:17:43 +0500

    everything changes now

--- airbnb/src/Propertylist.jsx
+++ airbnb/src/Propertylist.jsx
@@ -3,23 +3,27 @@ import { useEffect, useState } from 'react';
 import properties from './assets/property.json';
 import Card from './Card';
 import './css/Propertylist.css'
+import loading_img from './assets/loading.svg'
 
 export default function Propertylist() {
-    const [quanta, setQuanta] = useState();
-
-    useEffect(() => {
-        setQuanta(25);
-    }, [])
+    const [quanta, setQuanta] = useState(25);
+    const [loading, setLoading] = useState(false);
 
     const loadMore = () => {
-        setQuanta(prev => prev + 25);
-    };    
+        setLoading(true);
+        setTimeout(() => {
+            setQuanta(prev => prev + 25);
+            setLoading(false);
+        }, 1000);
+    };
+    
     return (
         <div className="card-container">
             {properties.slice(0, quanta).map((property, index) => (
                 <Card key={index} property={property} />
             ))}
-            {quanta < properties.length &&
+            {loading && <img src={loading_img} className="loading" />}
+            {!loading && quanta < properties.length &&
                 <button onClick={loadMore}>Show more</button>
             }
         </div>
